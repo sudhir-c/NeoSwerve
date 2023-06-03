41f44a6b1204a9d6f6e62b33f0fbbda114d89acf
Sudhir Chebiyyam <schebiyyam@hotmail.com>
2023-06-03 16:16:01 -0700

    Add basic top level logic

---
+++ src/main/java/frc/robot/Drive.java
@@ -0,0 +1,71 @@
+package frc.robot;
+
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Drive extends SubsystemBase {
+
+    private static final double WHEELBASE = 0;
+    private static final double TRACKWIDTH = 0;
+
+
+    //m/s
+    private final double maxVelocity;
+
+    private SwerveModuleState[] states = new SwerveModuleState[4];
+    private final SwerveModule[] modules;
+
+    private final SwerveDriveKinematics kinematics;
+
+    private final SwerveModulePosition[] positions;
+
+    private ChassisSpeeds targetVelocity = new ChassisSpeeds();
+
+    public Drive(
+            SwerveModuleIO frontLeft,
+            SwerveModuleIO frontRight,
+            SwerveModuleIO backLeft,
+            SwerveModuleIO backRight) {
+
+        modules = new SwerveModule[] {
+                new SwerveModule(frontLeft, "FrontLeft"),
+                new SwerveModule(frontRight, "FrontRight"),
+                new SwerveModule(backLeft, "BackLeft"),
+                new SwerveModule(backRight, "BackRight")
+        };
+        maxVelocity = frontLeft.getMaxVelocity();
+
+
+        positions = new SwerveModulePosition[] {
+                new SwerveModulePosition(),
+                new SwerveModulePosition(),
+                new SwerveModulePosition(),
+                new SwerveModulePosition()
+        };
+        var frontLeftLocation = new Translation2d(WHEELBASE / 2, TRACKWIDTH / 2);
+        var frontRightLocation = new Translation2d(WHEELBASE / 2, -TRACKWIDTH/2);
+        var backLeftLocation = new Translation2d(-WHEELBASE / 2 , TRACKWIDTH / 2);
+        var backRightLocation = new Translation2d(-WHEELBASE / 2 , -TRACKWIDTH / 2);
+
+        kinematics = new SwerveDriveKinematics(frontLeftLocation, frontRightLocation, backLeftLocation, backRightLocation);
+    }
+
+    @Override
+    public void periodic() {
+        SwerveModuleState[] optimized = new SwerveModuleState[4];
+        states = kinematics.toSwerveModuleStates(targetVelocity);
+        SwerveDriveKinematics.desaturateWheelSpeeds(states, maxVelocity);
+        for (int i = 0; i < optimized.length; i++) {
+            optimized[i] = SwerveModuleState.optimize(states[i], modules[i].getPosition().angle);
+            modules[i].setTargetState(optimized[i]);
+        }
+    }
+
+    public void setTargetVelocity(ChassisSpeeds target) {
+        targetVelocity = target;
+    }
+}
--- src/main/java/frc/robot/SwerveModule.java
+++ src/main/java/frc/robot/SwerveModule.java
@@ -1,13 +1,27 @@
 package frc.robot;
 
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+
 public class SwerveModule {
     private final SwerveModuleIO io;
     private final String name;
+    private final SwerveModuleIO.SwerveModuleIOInputs inputs = new SwerveModuleIO.SwerveModuleIOInputs();
 
     public SwerveModule(SwerveModuleIO io, String name) {
         this.io = io;
         this.name = name;
+    }
 
+    public void setTargetState(SwerveModuleState state) {
+
+    }
+    public SwerveModulePosition getPosition() {
+        return new SwerveModulePosition();
+    }
+
+    public void updateInputs() {
+        io.updateInputs(inputs);
     }
 
 }
